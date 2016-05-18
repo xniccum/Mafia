dbf4a884e9c0b95411b89488093132f3bb047bd6
Austin Tyler <tylerad@rose-hulman.edu>
2016-05-17 23:39:53 -0400

    Game logic implemented in Entity. Comments added to help explain the logic further.

--- app/src/main/java/com/squad/ana/mafia/Entity.java
+++ app/src/main/java/com/squad/ana/mafia/Entity.java
@@ -12,10 +12,16 @@ public class Entity {
     private Location location;
     private boolean hiding;
     private final int RANGE = 30;
+    private final int KILLRANGE = 5;
+    private boolean dead;
+    private int score;
+    private boolean sighted;
 
     public Entity(Location location) {
         this.location = location;
         this.hiding = true;
+        this.dead = false;
+        this.score = 0;
     }
 
     public boolean isHiding() { return hiding;}
@@ -23,6 +29,8 @@ public class Entity {
         this.location = location;
     }
     public Location getLocation() { return location;}
+    public int getScore() {return score;}
+    public boolean isTarget() { return sighted; }
 
     public void toggleHiding() { hiding = !hiding;}
 
@@ -36,6 +44,34 @@ public class Entity {
                 return true;
         }
         return false;
+    }
+
+    public void attack(List<Entity> players) {
+        float dist = RANGE;
+        Entity target = null;
+
+        for(Entity p: players) {
+            // loops through the list of known players to find the nearest one
+            if (this.location.distanceTo(p.getLocation()) < dist) {
+                dist = this.location.distanceTo(p.getLocation());
+                target = p;
+            }
+        }
+
+        if (dist < KILLRANGE && !target.hiding) {
+            // target is set to nearest player, they are in the kill range and they are not hiding
+                // this is where we set the target as the MAC address of the closest player,
+                //      assembling the "attack message" to be broadcast
+            target.die(); // this line will likely change, but I think it conveys the logic well,  the attacker is telling the target to die
+            this.score++;
+        }
+    }
+
+    public void die() {
+        // this is where we would handle our own death, as the target.
+            // In other words, our MAC address was received in the "attack message" broadcast
+        this.dead = true;
+        // Here is where we implement what happens when we die
     }
 
 
