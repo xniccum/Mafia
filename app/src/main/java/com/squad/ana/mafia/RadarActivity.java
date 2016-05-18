7721e0a5069798408f2a41c905597062fc2a2f36
Austin Niccum <xniccum@gmail.com>
2016-05-17 23:30:39 -0400

    further map progress

--- app/src/main/java/com/squad/ana/mafia/RadarActivity.java
+++ app/src/main/java/com/squad/ana/mafia/RadarActivity.java
@@ -1,13 +1,28 @@
 package com.squad.ana.mafia;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.plus.PlusOneButton;
 
 /**
@@ -18,110 +33,94 @@ import com.google.android.gms.plus.PlusOneButton;
  * Use the {@link RadarActivity#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class RadarActivity extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-    // The request code must be 0 or greater.
-    private static final int PLUS_ONE_REQUEST_CODE = 0;
-    // The URL to +1.  Must be a valid URL.
-    private final String PLUS_ONE_URL = "http://developer.android.com";
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-    private PlusOneButton mPlusOneButton;
+public class RadarActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private OnFragmentInteractionListener mListener;
-
-    public RadarActivity() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RadarActivity.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RadarActivity newInstance(String param1, String param2) {
-        RadarActivity fragment = new RadarActivity();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private GoogleMap mMap;
+    private Entity entity;
+    private Button fireBT;
+    private Button hideBT;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+        setContentView(R.layout.activity_radar);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        this.entity = new Entity(new Location(LocationManager.GPS_PROVIDER));
+
+        //get buttons
+        fireBT = (Button) findViewById(R.id.fireBT);
+        hideBT = (Button) findViewById(R.id.hideBT);
+
+        //add button listeners
+        fireBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //get all entities in range
+
+                //attempt to fire
+            }
+        });
+
+        hideBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //hide self on network
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.getUiSettings().setAllGesturesEnabled(false);
+
+        // Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                setLocation(location);
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+        beginLocationUpdates(locationListener);
+    }
+
+    private void beginLocationUpdates(LocationListener listener) {
+        // Acquire a reference to the system Location Manager
+        final LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        //Begin Location Updates If given permission by user
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==  PackageManager.PERMISSION_GRANTED) {
+            if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                // Register the listener with the Location Manager to receive location updates
+                locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,
+                        getResources().getInteger(R.integer.min_time),
+                        getResources().getInteger(R.integer.min_distance),
+                        listener);
+            }
+            else {
+                Log.d("Mafia-location","GPS Provider not enabled");
+            }
+        }
+        else {
+            Log.d("Mafia-location","Permission Not Granted");
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.activity_radar, container, false);
-
-        //Find the +1 button
-        mPlusOneButton = (PlusOneButton) view.findViewById(R.id.plus_one_button);
-
-        return view;
+    private void setLocation(Location loc) {
+        entity.setLocation(loc);
+        LatLng current = new LatLng(loc.getLatitude(), loc.getLongitude());
+        mMap.clear();
+        mMap.addMarker(new MarkerOptions().position(current));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(current));
     }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        // Refresh the state of the +1 button each time the activity receives focus.
-        mPlusOneButton.initialize(PLUS_ONE_URL, PLUS_ONE_REQUEST_CODE);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
-
 }
