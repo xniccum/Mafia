460c2ecd143e43c7bd74c0eda1ba3c8a2b23ce6d
Skornage <millerna@rose-hulman.edu>
2016-05-20 00:36:54 -0400

    merge

--- app/src/main/java/com/squad/ana/mafia/network/ReceiveAsyncTask.java
+++ app/src/main/java/com/squad/ana/mafia/network/ReceiveAsyncTask.java
@@ -3,6 +3,7 @@ package com.squad.ana.mafia.network;
 import android.app.Activity;
 import android.content.Context;
 import android.os.AsyncTask;
+import android.os.Environment;
 import android.widget.Toast;
 
 import com.squad.ana.mafia.engine.Engine;
@@ -10,10 +11,15 @@ import com.squad.ana.mafia.message.AttackMessage;
 import com.squad.ana.mafia.message.IProtocol;
 import com.squad.ana.mafia.message.UpdateMessage;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.URL;
 
 public class ReceiveAsyncTask extends AsyncTask<URL, Integer, Long> {
@@ -40,42 +46,19 @@ public class ReceiveAsyncTask extends AsyncTask<URL, Integer, Long> {
             socket.setBroadcast(true);
 
             while (true) {
-                ((Activity)context).runOnUiThread(new Runnable() {
-                    public void run() {
-                        Toast.makeText(context, "Ready to receive broadcast packets!", Toast.LENGTH_SHORT).show();
-                    }
-                });
 
                 //Receive a packet
                 byte[] recvBuf = new byte[15000];
                 final DatagramPacket packet = new DatagramPacket(recvBuf, recvBuf.length);
                 socket.receive(packet);
-
-                //Packet received
-                ((Activity)context).runOnUiThread(new Runnable() {
-                    public void run() {
-                        Toast.makeText(context, "Packet received from: " + packet.getAddress().getHostAddress(), Toast.LENGTH_SHORT).show();
-                    }
-                });
                 final String data = new String(packet.getData()).trim();
                 IProtocol request = parseRequestString(data);
 
                 if (request.getType().equals(IProtocol.UPDATE)) {
-                    if(!((UpdateMessage)request).getSrc().equals(Engine.getMacAddress())) {
+                    if(!((UpdateMessage)request).getSrc().toLowerCase().equals(Engine.getMacAddress().toLowerCase())) {
                         Engine.updatePlayers((UpdateMessage)request);
                     }
-                } else {
-                    if(((AttackMessage)request).getTarget().equals(Engine.getMacAddress())) {
-                        Engine.die();
-                    }
                 }
-
-
-                ((Activity)context).runOnUiThread(new Runnable() {
-                    public void run() {
-                        Toast.makeText(context, "Packet received; data: " + data, Toast.LENGTH_SHORT).show();
-                    }
-                });
 
                 // Send the packet data back to the UI thread
 //                Intent localIntent = new Intent(Constants.BROADCAST_ACTION)
@@ -94,22 +77,13 @@ public class ReceiveAsyncTask extends AsyncTask<URL, Integer, Long> {
         return null;
     }
 
-//    private String readInput(InputStream inputStream) throws IOException {
-//        BufferedReader r = new BufferedReader(new InputStreamReader(inputStream));
-//        StringBuilder total = new StringBuilder();
-//        String line;
-//        while ((line = r.readLine()) != null) {
-//            total.append(line).append('\n');
-//        }
-//        return total.toString();
-//    }
-
     public IProtocol parseRequestString(String requestString) {
         String[] fields = requestString.split("\n");
+        System.out.println(fields[0]);
         IProtocol protocol = null;
         String[][] fieldPairs = new String[fields.length][];
         for (int i = 0; i < fields.length; i++) {
-            fieldPairs[i] = fields[i].split(":");
+            fieldPairs[i] = fields[i].split(";");
         }
 
         String fieldHeader = fieldPairs[0][0].trim();
@@ -125,7 +99,8 @@ public class ReceiveAsyncTask extends AsyncTask<URL, Integer, Long> {
                 double latitude = Double.parseDouble(location[0]);
                 double longitude = Double.parseDouble(location[1]);
                 ((UpdateMessage) protocol).setLocation(new double[]{latitude, longitude});
-                ((UpdateMessage) protocol).setHidden(Boolean.parseBoolean(fieldPairs[3][1]));
+                String hidden = fieldPairs[3][1];
+                ((UpdateMessage) protocol).setHidden(Boolean.parseBoolean(hidden.trim()));
             }
         }
         return protocol;
